d79cac5d90521f3cf2ab39d260a2ebbfd142de50
xopxrd <92743266+xopxrd@users.noreply.github.com>
2023-04-19 15:44:49 +0700

    Add files via upload

--- new/src/Main.cpp
+++ new/src/Main.cpp
@@ -30,36 +30,26 @@ int main(int argc, char *argv[])
 	IMG_Init(IMG_INIT_PNG);
 	TTF_Init();
 
-	Render* window = new Render("new", SCREEN_WIDTH, SCREEN_HEIGHT);
+	Render* window = new Render("Cat Jump", SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	TTF_Font* font32 = TTF_OpenFont("../../new/res/fonts/cocogoose.ttf", 32);
-	TTF_Font* font32_outline = TTF_OpenFont("../../new/res/fonts/cocogoose.ttf", 32);
-	TTF_Font* font24 = TTF_OpenFont("../../new/res/fonts/cocogoose.ttf", 24);
-	TTF_Font* font16 = TTF_OpenFont("../../new/res/fonts/cocogoose.ttf", 16);
+	TTF_Font* font32 = TTF_OpenFont("../../Cat Jump/res/fonts/cocogoose.ttf", 32);
+	TTF_Font* font32_outline = TTF_OpenFont("../../Cat Jump/res/fonts/cocogoose.ttf", 32);
+	TTF_Font* font24 = TTF_OpenFont("../../Cat Jump/res/fonts/cocogoose.ttf", 24);
+	TTF_Font* font16 = TTF_OpenFont("../../Cat Jump/res/fonts/cocogoose.ttf", 16);
 	TTF_SetFontOutline(font32_outline, 3); 
 
-	SDL_Texture* playerTex = window->loadTexture("../../new/res/textures/cat.png");
-	SDL_Texture* blockTex = window->loadTexture("../../new/res/textures/block.png");
-	SDL_Texture* logo = window->loadTexture("../../new/res/textures/logo.png");
-	SDL_Texture* deathOverlay = window->loadTexture("../../new/res/textures/death_overlay.png");
-
-	SDL_Texture* author = window->loadTexture("xopxrd", font24, white);
-	SDL_Texture* startMessage = window->loadTexture("Click to start", font24, white);
-	SDL_Texture* deathMessage1 = window->loadTexture("You die...", font24, white);
-	SDL_Texture* deathMessage2 = window->loadTexture("Press space to retry", font16, white);
+	SDL_Texture* playerTex = window->loadTexture("../../Cat Jump/res/textures/cat.png");
+	SDL_Texture* blockTex = window->loadTexture("../../Cat Jump/res/textures/block.png");
+	SDL_Texture* logo = window->loadTexture("../../Cat Jump/res/textures/logo.png");
+	SDL_Texture* deathOverlay = window->loadTexture("../../Cat Jump/res/textures/death_overlay.png");
 
 	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
 		std::cout << "error: " << Mix_GetError() << std::endl;
 
-	Mix_Music* backgroundMusic = Mix_LoadMUS("../../new/res/sound/Wallpaper.mp3");
-	Mix_Chunk* fallSound = Mix_LoadWAV("../../new/res/sound/mixkit-falling-hit-on-gravel-756.wav");
+	Mix_Music* backgroundMusic = Mix_LoadMUS("../../Cat Jump/res/sound/Wallpaper.mp3");
+	Mix_Chunk* fallSound = Mix_LoadWAV("../../Cat Jump/res/sound/mixkit-falling-hit-on-gravel-756.wav");
 
 	Player* cat = new Player(0, SCREEN_HEIGHT - 32, playerTex);
-
-	SDL_Texture* score = window->loadTexture(cat->getScore(), font32, white);
-	SDL_Texture* scoreOutline = window->loadTexture(cat->getScore(), font32_outline, black);
-	SDL_Texture* highscore = window->loadTexture(cat->getHighscore(), font32, white);
-
 	Block* emptyBlock[10];
 	int counter = 0;
 
@@ -109,14 +99,14 @@ int main(int argc, char *argv[])
 			if (SDL_GetTicks() < 2500)
 			{
 				window->clear();
-				window->renderCenter(0, sin(SDL_GetTicks()/100) * 2 - 4, author);
+				window->renderCenter(0, sin(SDL_GetTicks()/100) * 2 - 4, "xopxrd", font24, white);
 				window->display(); // update screen
 			}
 			else
 			{
 				window->clear();
 				window->render(0, 0, logo);
-				window->renderCenter(0, 90 + sin(SDL_GetTicks()/100) * 2, startMessage);
+				window->renderCenter(0, 90 + sin(SDL_GetTicks()/100) * 2 - 4, "Click to start", font24, white);
 				window->display();
 			}
 		}
@@ -133,9 +123,9 @@ int main(int argc, char *argv[])
 			for(int i = 0; i < 10; i++)
 				window->render(*emptyBlock[i]);
 
-			// window->ttfLoad(0, 0, font32_outline, cat->getScore(), black);
-			// window->ttfLoad(3, 3, font32, cat->getScore(), white);
-			// window->ttfLoad(3, 44, font16, cat->getHighscore(), white);
+			window->render(0, 0, cat->getScore(), font32_outline, black);
+			window->render(3, 3, cat->getScore(), font32, white);
+			window->render(3, 44, cat->getHighscore(), font16, white);
 
 			if (cat->getCondition() == PLAY)
 				playedDeathSFX = false;
@@ -144,8 +134,8 @@ int main(int argc, char *argv[])
 			{
 				Mix_HaltMusic();
 				window->render(0, 0, deathOverlay);
-				window->renderCenter(0, -24, deathMessage1);
-				window->renderCenter(0, 12, deathMessage2);
+				window->renderCenter(0, -24, "You die...", font24, white);
+				window->renderCenter(0, 12, "Press space to retry", font16, white);
 				if (!playedDeathSFX)
 				{
 					Mix_PlayChannel(1, fallSound, 0);
